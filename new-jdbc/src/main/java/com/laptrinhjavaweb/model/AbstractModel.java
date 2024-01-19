2a16ad44755af87ea389c7a3b66f5a0a416a9d7c
Trung <vuquangtrung098765@gmail.com>
2024-01-19 19:04:22 +0700

    first commit

---
+++ new-jdbc/src/main/java/com/laptrinhjavaweb/model/AbstractModel.java
@@ -0,0 +1,41 @@
+package com.laptrinhjavaweb.model;
+
+import com.google.protobuf.Timestamp;
+
+public class AbstractModel {
+	private Long id;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public Timestamp getCreatedDate() {
+		return createdDate;
+	}
+	public void setCreatedDate(Timestamp createdDate) {
+		this.createdDate = createdDate;
+	}
+	public Timestamp getModifiedDate() {
+		return modifiedDate;
+	}
+	public void setModifiedDate(Timestamp modifiedDate) {
+		this.modifiedDate = modifiedDate;
+	}
+	public String getCreatedBy() {
+		return createdBy;
+	}
+	public void setCreatedBy(String createdBy) {
+		this.createdBy = createdBy;
+	}
+	public String getModifiedBy() {
+		return modifiedBy;
+	}
+	public void setModifiedBy(String modifiedBy) {
+		this.modifiedBy = modifiedBy;
+	}
+	private Timestamp createdDate;
+	private Timestamp modifiedDate;
+	private String createdBy;
+	private String modifiedBy;
+}
